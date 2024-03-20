af506f2524733b2e17b8935fb05328fb356f239e
FalAut <143469089+FalAut@users.noreply.github.com>
2024-03-20 12:49:25 +0800

    曲奇法杖：消耗背包里4个曲奇碎屑，随机地放置曲奇方块

--- kubejs/server_scripts/Events/ClickEvents.js
+++ kubejs/server_scripts/Events/ClickEvents.js
@@ -5,3 +5,24 @@ BlockEvents.rightClicked("minecraft:chest", (event) => {
         // event.player.paint({last_message: {text: `Last message: ${event.block.getInventory().getAllItems()}`}})
     }
 })
+
+ItemEvents.rightClicked("cookies:cookie_wand", (event) => {
+    const { target } = event
+    const CookieBlocks = [
+        "cookies:cookie_block1",
+        "cookies:cookie_block2",
+        "cookies:cookie_block3",
+        "cookies:cookie_block4",
+        "cookies:cookie_block5",
+        "cookies:cookie_block6",
+        "cookies:cookie_block7",
+    ]
+
+    event.player.inventory.allItems.forEach((item) => {
+        if (item.id == "cookies:cookie_chip") {
+            const randomIndex = Math.floor(Math.random() * CookieBlocks.length)
+            target.block[target.facing].set(CookieBlocks[randomIndex])
+            item.count -= 4
+        }
+    })
+})
